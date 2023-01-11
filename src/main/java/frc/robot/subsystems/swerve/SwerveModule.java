6aec77cae7c3124bc25f66f4bb2108be7ed7508f
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-01-11 13:28:47 -0600

    Removed duplicate optimization.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -633,8 +633,8 @@ public class SwerveModule<DriveMotorType extends MotorController, AngleMotorType
    */
   public void setAngle(double angle)
   {
-    angle = SwerveModuleState.optimize(getState(AbsoluteSensorRange.Signed_PlusMinus180),
-                                       Rotation2d.fromDegrees(angle)).angle.getDegrees();
+//    angle = SwerveModuleState.optimize(getState(AbsoluteSensorRange.Signed_PlusMinus180),
+//                                       Rotation2d.fromDegrees(angle)).angle.getDegrees();
 
     angle += angle < 0 ? 180 : 0; // Ensure angle is always given within range of 0 to 360.
     assert angle <= 360;
